d9bd37869dc8bed5411c3d797ec91464c55b7cf8
gabriel <gabriel.ryuji.konishi@gmail.com>
2025-06-02 13:23:56 +0900

    insertion of the variables class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
     println("Hello World!")
+    printVariables()
+    constantVariables()
+}
+
+fun printVariables() {
+    var variable = "here we can mutate the variable"
+    print(variable + " ")
+    variable = "by overwritting the previous one"
+    println(variable)
+}
+
+fun constantVariables() {
+    val const = "here a constant variable"
+    println(const)
 }
